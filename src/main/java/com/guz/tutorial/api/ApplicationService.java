50899086fbd139db7cd011fb440d7f1df52b4dda
Guz Yury <Iurii_Guz@epam.com>
2019-05-21 17:44:34 +0300

    Initial commit

---
+++ src/main/java/com/guz/tutorial/api/ApplicationService.java
@@ -0,0 +1,11 @@
+package com.guz.tutorial.api;
+
+/**
+ * ApplicationService.
+ *
+ * @author Iurii_Guz
+ */
+public interface ApplicationService {
+
+    void doSomeApplicationUsecase();
+}
---
+++ src/main/java/com/guz/tutorial/impl/ApplicationServiceImpl.java
@@ -0,0 +1,15 @@
+package com.guz.tutorial.impl;
+
+import com.guz.tutorial.api.ApplicationService;
+
+/**
+ * ApplicationServiceImpl.
+ *
+ * @author Iurii_Guz
+ */
+public class ApplicationServiceImpl implements ApplicationService {
+
+    public void doSomeApplicationUsecase() {
+        // do some logic
+    }
+}
